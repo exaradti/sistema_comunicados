cf9037b50d91cdab561dc298a76218e1cfc1c0a2
exaradti <exaradti@gmail.com>
2026-04-17 11:13:02 -0300

    Delete lib directory

--- lib/hash_password.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs'
-
-export async function gerar_senha_hash(senha: string) {
-  return bcrypt.hash(senha, 10)
-}
-
-export async function validar_senha_hash(senha: string, senha_hash: string) {
-  return bcrypt.compare(senha, senha_hash)
-}
