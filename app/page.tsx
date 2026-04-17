4d07215f28704a0cdb93b68193aecfa183e17374
exaradti <exaradti@gmail.com>
2026-04-17 11:11:58 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-
-export default async function HomePage() {
-  const has_session = (await cookies()).get('admin_session')?.value
-  redirect(has_session ? '/dashboard' : '/login')
-}
